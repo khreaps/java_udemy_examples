9e3f9b06c06f96239a7ac377e12cc394ba0618da
Ricardo <ricardo.galvez@gmail.com>
2020-10-10 00:24:43 -0500

    Udemy Java Course: examples

---
+++ KeywordsAndExpressions/src/KeywAndExpressions1.java
@@ -0,0 +1,12 @@
+public class KeywAndExpressions1 {
+    
+    public static void main(String[] args) {
+        
+        // 1 mile = 1.60933 km
+        double kilometres = (100 * 1.60933);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
---
+++ KeywordsAndExpressions/src/KeywAndExpressions2.java
@@ -0,0 +1,15 @@
+public class KeywAndExpressions2 {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" + " another" + " still more");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one");
+
+    }
+}
