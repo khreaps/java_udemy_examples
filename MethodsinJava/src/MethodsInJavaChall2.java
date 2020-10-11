26b9dab2d92ad2a41426c933c6a65947c027479a
Ricardo <ricardo.galvez@gmail.com>
2020-10-11 11:22:27 -0500

    Udemy Java Course: examples

--- MethodsinJava/src/MethodsInJavaChall2.java
+++ MethodsinJava/src/MethodsInJavaChall2.java
@@ -1,9 +1,10 @@
-public class MethodsInJavaChall {
+public class MethodsInJavaChall2 {
 
     public static void main(String[] args) {
 
         int highScore = 0;
         int numPosition = 0;
+
         highScore = calculateScore(true, 1000, 1, 0);
         System.out.println("Your final score was " + highScore);
         numPosition = calculateHighScorePosition(highScore);
@@ -22,15 +23,26 @@ public class MethodsInJavaChall {
 
     public static int calculateHighScorePosition(int playerScore) {
 
+        // if (playerScore >= 1000) {
+        //     return 1;
+        // } else if (playerScore >= 500) {
+        //     return 2;
+        // } else if (playerScore >= 100) {
+        //     return 3;
+        // }
+        // return 4;
+        
+        int position = 4;   // assuming position 4 will be returned
+
         if (playerScore >= 1000) {
-            return 1;
+            position = 1;
         } else if (playerScore >= 500) {
-            return 2;
+            position =2;
         } else if (playerScore >= 100) {
-            return 3;
-        } else {
-            return 4;
+            position = 3;
         }
+
+        return position;
     }
 
     public static void displayHighScorePosition(String namePlayer, int numPosition) {
