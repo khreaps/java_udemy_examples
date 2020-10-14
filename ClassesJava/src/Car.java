29a161a7e5c5c997d3169f223489d1abbc49ece8
Ricardo <ricardo.galvez@gmail.com>
2020-10-14 18:26:29 -0500

    Udemy Java Course: examples

---
+++ ClassesJava/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model){
+        this.model = model;
+    } 
+}
